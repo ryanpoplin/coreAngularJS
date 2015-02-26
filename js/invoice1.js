4f0e3311389948f65077edb9967f1fd1aa051ceb
Byrdann Fox <mobileupstate@gmail.com>
2015-02-26 14:49:10 -0500

    AngularJS currency API app testing...

--- js/invoice1.js
+++ js/invoice1.js
@@ -1,25 +1,21 @@
-angular.module('invoice1', []).controller('InvoiceController', function () {
+// the 'invoice1' module depends on the 'finance' module for proper functionality...
+// this will in turn utilize the 'InvoiceController' and 'currencyConverter' services...
+angular.module('invoice1', ['finance']).controller('InvoiceController', ['currencyConverter', function (currencyConverter) {
+
 	this.qty = 1;
-	this.cost = 2;
-	this.inCurr = 'EUR';
-	this.currencies = ['USD', 'EUR', 'CNY'];
-	this.usdToForeignRates = {
-		// ever changing...
-		USD: 1,
-		EUR: 0.74,
-		CNY: 6.09
-	};
-
+	
+	this.cost = 1;
+	
+	this.inCurr = 'USD';
+	
+	this.currencies = currencyConverter.currencies;
+	
 	this.total = function total (outCurr) {
-		return this.convertCurrency(this.qty * this.cost, this.inCurr, outCurr);
+		return currencyConverter.convert(this.qty * this.cost, this.inCurr, outCurr);
 	};
-
-	this.convertCurrency = function convertCurrency (amount, inCurr, outCurr) {
-		return amount * this.usdToForeignRates[outCurr] / this.usdToForeignRates[inCurr];
-	};
-
+	
 	this.pay = function pay () {
-		window.alert("Thanks...");
+		window.alert('...');
 	};
 
-});
+}]);	
