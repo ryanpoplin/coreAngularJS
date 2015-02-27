47d74208bc887bc5f0df09a098eafa435e31c2ee
Byrdann Fox <mobileupstate@gmail.com>
2015-02-26 20:21:20 -0500

    ...

--- blogAppTesting/controllers/controller.js
+++ blogAppTesting/controllers/controller.js
@@ -1,14 +1,47 @@
 // 'ControllerModule' === module name...
 // [] === module dependencies...such as: other modules, services, etc...
 // method chaining feature === .controller, etc... different methods of the module...
-angular.module('ControllerModule', []).controller('HeaderController', function ($scope) {
+angular.module('HeaderModule', []).factory("Data", function () {
+
+	return {
+
+		getTitle: function () {
+
+			return "The Best Weather App Ever...";
+
+		}
+
+	}
+
+}).filter('customuppercase', function() {
+
+	// the custom data structure and algorithm layer...
+
+	return function (input) {
+
+		if (input.length > 20) {
+
+			return input.toUpperCase();
+
+		} else {
+
+			return input;
+
+		}
+
+	}
+
+// module method chaining is fun...
+}).controller('HeaderController', function ($scope, Data) {
 
 	// are $scope and this interchangeable???
 
-	$scope.title = "Weather App...";
+	$scope.title = Data.getTitle();
 
 	$scope.updateTitle = function () {
+		
 		$scope.title = "New Weather App...";
+	
 	};
 
 });
